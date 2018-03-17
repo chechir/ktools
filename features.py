bf807d837c8ce69aad4e9c0aa4d207401b199b3b
maggi <a@a.com>
2018-03-17 13:05:05 +0000

    added lagged_values with 2 dfs and tests

--- features.py
+++ features.py
@@ -22,17 +22,16 @@ def _lagged_ratio(v, mask_nans=True):
     return result
 
 
-def lagged_values_by_group(df, groupby, calc_colname, shift, fillna=-1):
+def lagged_values_by_group(df, col, groupby, shift, fillna=-1):
     lag_func = partial(tools.lag, init=np.nan, shift=shift)
-    values = tools.group_apply(df[calc_colname].values, df[groupby].values, lag_func)
+    values = tools.group_apply(df[col].values, df[groupby].values, lag_func)
     values[~np.isfinite(values)] = fillna
     return values
 
 
 def lagged_ratio_smoothing(df, col, groupby, smoothing_value=1,
                            min_samples_leaf=5, prior=0, condition_col=None):
-    """
-    Smoothing is computed like in the following paper by Daniele Micci-Barreca
+    """ Smoothing is computed like in the following paper by Daniele Micci-Barreca
     https://kaggle2.blob.core.windows.net/forum-message-attachments/225952/7441/high%20cardinality%20categoricals.pdf
     min_samples_leaf (int) : minimum samples to take category average into account
     smoothing (int) : smoothing effect to balance categorical average vs prior
@@ -86,3 +85,22 @@ def ratio_smoothing(df_train, df_test, col, groupby, smoothing_value=1,
     # pd.merge does not keep the index so restore it
     df_test_result.index = df_test.index
     return df_test_result.values
+
+
+def units_to():
+    pass
+
+
+def lagged_values_by_group_2dfs(df_train, df_test, col,
+                                groupby, date_col, fillna=-1):
+    df_test_temp = df_test[[date_col, groupby]]
+    df_train_temp = df_train[[groupby, date_col, col]]
+    # Compute last value for train_df
+    max_dates = df_train[[groupby, date_col]].groupby(by=groupby)[date_col].agg(['max'])
+    max_dates = max_dates.reset_index().rename(columns={'index': groupby, 'max': date_col})
+    df_train_temp = max_dates.merge(df_train, how='inner', on=[groupby, date_col])
+    # Merge with test using the groupby col:
+    df_test_result = df_test_temp.merge(df_train_temp, on=groupby, how='left').reset_index()
+    df_test_result = df_test_result.fillna(fillna)
+    return df_test_result[col].values
+
--- tests/test_features.py
+++ tests/test_features.py
@@ -1,4 +1,5 @@
 from ktools import features
+from ktools import tools
 import pandas as pd
 import numpy as np
 
@@ -26,4 +27,36 @@ def test_lagged_ratio_smoothing():
     result = features.lagged_ratio_smoothing(df, 'saleprice_log', 'group', prior=12.4,
                                              min_samples_leaf=1)
     expected = [12.4, 12.2, 12.83863515, 12.4]
+    assert(np.allclose(result, expected))
+
+
+def test_lagged_values_by_group():
+    df = pd.DataFrame({
+        # 'year': [2012, 2013, 2014, 2015],
+        'saleprice_log': [12, 14, 15, 3],
+        'group': [1, 1, 1, 2]
+        })
+    result = features.lagged_values_by_group(df, 'saleprice_log', 'group', 1)
+    expected = [-1, 12, 14, -1]
+    assert(np.allclose(result, expected))
+
+
+def test_lagged_values_two_dfs():
+    ''' returns the features only for the test '''
+    df_train = pd.DataFrame({
+        'recordingdate': ['2012-01-01', '2012-02-02', '2012-02-03', '2013-01-01'],
+        'saleprice_log': [12, 14, 15, 3],
+        'group': [1, 1, 1, 2]
+        })
+    df_train['recordingdate'] = pd.to_datetime(df_train['recordingdate'])
+    df_test = pd.DataFrame({
+        'recordingdate': ['2014-01-01', '2016-02-02', '2012-06-03'],
+        'saleprice_log': [15, 3, -1],
+        'group': [1, 2, 3]
+        })
+    result = features.lagged_values_by_group_2dfs(
+            df_train, df_test,
+            'saleprice_log',
+            'group', 'recordingdate', fillna=-1)
+    expected = [15, 3, -1]
     assert(np.allclose(result, expected))
